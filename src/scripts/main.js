d6b43b9d6d4568c0128fdaf672f5f5630f9b887c
Trent Templet <tntemplet@gmail.com>
2024-04-01 14:28:28 -0500

    Cleaning up Broken Glass

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,13 +22,6 @@ function createTable(division,age,url) {
                 .then(res => res.text())
                 .then(rep => {
                     const data = JSON.parse(rep.substr(47).slice(0, -2));
-                    // const row = document.createElement('tr');
-                    // header.append(row);
-                    // data.table.cols.forEach((heading) => {
-                    //     const cell = document.createElement('td');
-                    //     cell.textContent = heading.label;
-                    //     row.append(cell);
-                    // });
                     data.table.rows.forEach((main) => {
                         const container = document.createElement('tr');
                         output.append(container);
